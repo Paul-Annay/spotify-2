4d210ee175f186b57df9e9449360a8856e1ec593
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-07-26 20:43:30 +0600

    * API: Created the spotifyApi * Auth:     - Using the SpotifProvider created authOptions for NextAuth     - implemented refreshAccessToken to handle accesstoken rotation logic     - using the callbacks attribute implemented the jwt function which handles the refreshing logic for the access token

--- README.md
+++ README.md
@@ -36,3 +36,7 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 ## Implementing Next Auth
 
 To Get started with [Next Auth](https://next-auth.js.org/getting-started/example) use the template
+
+## Implemneting the Token Refresh Logic
+
+Read [Refresh Token Rotation](https://next-auth.js.org/v3/tutorials/refresh-token-rotation)
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,28 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
+
+async function refreshAccessToken(token) {
+    try {
+        spotifyApi.setAccessToken(token.accessToken);
+        spotifyApi.setRefreshToken(token.refreshToken);
+
+        const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+
+        return {
+            ...token,
+            accessToken: refreshedToken.access_token,
+            accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000,
+            refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            ...token,
+            error: "RefreshAccessTokenError",
+        };
+    }
+}
 
 export const authOptions = {
     // Configure one or more authentication providers
@@ -7,9 +30,43 @@ export const authOptions = {
         SpotifyProvider({
             clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
             clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+            authorization: LOGIN_URL,
         }),
         // ...add more providers here
     ],
+    secret: process.env.JWS_SECRET,
+    pages: {
+        signIn: "/login",
+    },
+    callbacks: {
+        async jwt(token, user, account) {
+            // Initial sign in
+            if (account && user) {
+                return {
+                    ...token,
+                    accessToken: account.access_token,
+                    accessTokenExpires: account.expires_in * 1000,
+                    refreshToken: account.refresh_token,
+                    username: account.providerAccountId,
+                };
+            }
+
+            // Return previous token if the access token has not expired yet
+            if (Date.now() < token.accessTokenExpires) {
+                return token;
+            }
+
+            // Access token has expired, try to update it
+            return await refreshAccessToken(token);
+        },
+    },
+    async session({ session, token }) {
+        session.user.accessToken = token.accessToken;
+        session.user.refreshToken = token.refreshToken;
+        session.user.username = token.username;
+
+        return session;
+    },
 };
 
 export default NextAuth(authOptions);
